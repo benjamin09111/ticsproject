0d14ce7106e080d663db26750b39f97b3200a571
Benjamin Morales <benjamin.morales3@mail.udp.cl>
2023-11-21 18:43:59 -0300

    a

--- public/js/fill.js
+++ public/js/fill.js
@@ -3,41 +3,49 @@ const loginForm = document.getElementById('lgn');
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault(); // Evita que se envíe el formulario de manera convencional
 
-    const cookieString = document.cookie;
-    const cookies = cookieString.split('; ');
+    const espacio1 = document.querySelector('input[name="espacio1"]');
+    const espacio2 = document.querySelector('input[name="espacio2"]');
+    const espacio3 = document.querySelector('input[name="espacio3"]');
 
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].split('=');
-        const cookieName = cookie[0];
-        const cookieValue = cookie[1];
+    // Para los elementos relacionados con espacio 1
+const max1 = document.querySelector('input[name="max1"]');
+const d1 = document.querySelector('input[name="d1"]');
 
-        if (cookieName === 'token') {
-            token = cookieValue;
-            break;
-        }
-    }
+// Para los elementos relacionados con espacio 2
+const max2 = document.querySelector('input[name="max2"]');
+const d2 = document.querySelector('input[name="d2"]');
 
-    if(token == null || !token){
-        window.location.href = "/";
-    }else{
-        fetch('https://ticsproject.onrender.com/fill', {
-        method: 'GET',
+// Para los elementos relacionados con espacio 3
+const max3 = document.querySelector('input[name="max3"]');
+const d3 = document.querySelector('input[name="d3"]');
+
+    fetch('https://ticsproject.onrender.com/fill', {
+        method: 'POST',
         headers: {
-            'token': token,
             'Content-Type': 'application/json'
         },
+        body: JSON.stringify({
+            espacio1: espacio1,
+            espacio2: espacio2,
+            espacio3: espacio3,
+            max1: max1,
+            max2: max2,
+            max3: max3,
+            d1: d1,
+            d2: d2,
+            d3: d3
+        })
     })
         .then(response => response.json())
         .then(data => {
             if (data.success === 'true') {
-                window.location.href = '/home';
+                windows.location.href = "/home"
             } else {
-                windows.location.href = '/fallo';
+                windows.location.href = "/fallo"
             }
         })
         //dejar otro error naturaleza
         .catch(error => {
             window.location.href = '/fallo';
         });
-    }
 });
